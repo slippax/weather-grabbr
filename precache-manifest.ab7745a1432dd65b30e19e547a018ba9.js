99509f193f002b1bec79a9ef76e52aed7e80b0a0
Stephen Lippa <you@example.com>
2020-07-30 11:38:00 -0400

    Updates

---
+++ precache-manifest.ab7745a1432dd65b30e19e547a018ba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce38ef18782271b937f7a48a5ac507e",
+    "url": "/weather-grabbr/index.html"
+  },
+  {
+    "revision": "c825f69639273197eef6",
+    "url": "/weather-grabbr/static/css/main.2e6710d9.chunk.css"
+  },
+  {
+    "revision": "db36cc1c412c9a1c0306",
+    "url": "/weather-grabbr/static/js/2.16b97446.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-grabbr/static/js/2.16b97446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c825f69639273197eef6",
+    "url": "/weather-grabbr/static/js/main.bff033bf.chunk.js"
+  },
+  {
+    "revision": "9af11be6d8fb9bdfdf96",
+    "url": "/weather-grabbr/static/js/runtime-main.2424f59c.js"
+  }
+]);
